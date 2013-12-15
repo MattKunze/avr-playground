2a3b8a8c9a7ecaa31cc5d55cc8dd6e7587cbd761
Matt Kunze <matt.kunze@datasplice.com>
2013-12-15 13:16:38 -0700

    working on sample tx app
    compiles, but doesn't work for some reason

--- mirf/spi.c
+++ mirf/spi.c
@@ -26,63 +26,37 @@
 
 #include "spi.h"
 
-#include <avr/io.h>
-#include <avr/interrupt.h>
-
-#define PORT_SPI    PORTB
-#define DDR_SPI     DDRB
-#define DD_MISO     DDB4
-#define DD_MOSI     DDB3
-#define DD_SS       DDB2
-#define DD_SCK      DDB5
-
-
-void spi_init()
-  // Initialize pins for spi communication
-{
-  DDR_SPI &= ~((1<<DD_MOSI)|(1<<DD_MISO)|(1<<DD_SS)|(1<<DD_SCK));
-  // Define the following pins as output
-  DDR_SPI |= ((1<<DD_MOSI)|(1<<DD_SS)|(1<<DD_SCK));
-
-
-  SPCR = ((1<<SPE)|               // SPI Enable
-      (0<<SPIE)|              // SPI Interupt Enable
-      (0<<DORD)|              // Data Order (0:MSB first / 1:LSB first)
-      (1<<MSTR)|              // Master/Slave select   
-      (0<<SPR1)|(1<<SPR0)|    // SPI Clock Rate
-      (0<<CPOL)|              // Clock Polarity (0:SCK low / 1:SCK hi when idle)
-      (0<<CPHA));             // Clock Phase (0:leading / 1:trailing edge sampling)
-
-  SPSR = (1<<SPI2X);              // Double Clock Rate
-
+// Initialise the SPI
+void spi_init(void) {
+	DDRB |= (1<<PB2); // SPI CLK
+	DDRB |= (1<<PB1); // SPI DO
+	DDRB &= ~(1<<PB0); // SPI DI
+	PORTB |= (1<<PB0); // SPI DI
 }
 
-void spi_transfer_sync (uint8_t * dataout, uint8_t * datain, uint8_t len)
-  // Shift full array through target device
-{
-  uint8_t i;      
-  for (i = 0; i < len; i++) {
-    SPDR = dataout[i];
-    while((SPSR & (1<<SPIF))==0);
-    datain[i] = SPDR;
-  }
+// SPI transfer 1 byte and return the result
+uint8_t spi_transfer(uint8_t data) {
+	USIDR = data;
+	USISR = _BV(USIOIF); // clear flag
+
+	while ( (USISR & _BV(USIOIF)) == 0 ) {
+		USICR = (1<<USIWM0)|(1<<USICS1)|(1<<USICLK)|(1<<USITC);
+	}
+	return USIDR;
 }
 
-void spi_transmit_sync (uint8_t * dataout, uint8_t len)
-  // Shift full array to target device without receiving any byte
-{
-  uint8_t i;      
-  for (i = 0; i < len; i++) {
-    SPDR = dataout[i];
-    while((SPSR & (1<<SPIF))==0);
-  }
+// Write data using SPI
+void spi_write_data(uint8_t * dataout, uint8_t len) {
+	uint8_t i;
+	for (i = 0; i < len; i++) {
+		spi_transfer(dataout[i]);
+	}
 }
 
-uint8_t spi_fast_shift (uint8_t data)
-  // Clocks only one byte to target device and returns the received one
-{
-  SPDR = data;
-  while((SPSR & (1<<SPIF))==0);
-  return SPDR;
+// Read data using SPI
+void spi_read_data(uint8_t * datain, uint8_t len) {
+	uint8_t i;
+	for (i = 0; i < len; i++) {
+		datain[i] = spi_transfer(0x00);
+	}
 }
-
--- mirf/spi.h
+++ mirf/spi.h
@@ -29,11 +29,9 @@
 
 #include <avr/io.h>
 
-
-extern void spi_init();
-extern void spi_transfer_sync (uint8_t * dataout, uint8_t * datain, uint8_t len);
-extern void spi_transmit_sync (uint8_t * dataout, uint8_t len);
-extern uint8_t spi_fast_shift (uint8_t data);
-
+extern void spi_init(void);
+extern uint8_t spi_transfer(uint8_t data);
+extern void spi_write_data(uint8_t * dataout, uint8_t len);
+extern void spi_read_data(uint8_t * datain, uint8_t len);
 
 #endif /* _SPI_H_ */
